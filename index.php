788a316a9941dfabe749e402b60fd1abfa477674
Emmanuel - Net2M <emmanuel@net2m.fr>
2015-06-01 15:22:22 +0200

    Création du fichier d'index du projet et de son titre

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <title>Projet OCC Git - GitHub</title>
+    </head>
+
+    <body>
+        <h1>Projet OCC Git - GitHub</h1>
+    </body>
+</html>
