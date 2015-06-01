c4c1f05b88e6c09dc7240818611fd9c2e39d3d7a
Emmanuel - Net2M <emmanuel@net2m.fr>
2015-06-01 15:45:26 +0200

    Ajout d'un texte sous le titre

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 
     <body>
         <h1>Projet OCC Git - GitHub</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula id est id tincidunt. Donec consequat porta nisl ut scelerisque. Integer feugiat imperdiet lorem non tincidunt. Aenean euismod dui neque, eu congue odio tincidunt eu. Maecenas imperdiet vulputate aliquet. Mauris facilisis id magna id feugiat. Sed vel massa tortor. Donec varius sodales sapien viverra vestibulum. </p>
     </body>
 </html>
